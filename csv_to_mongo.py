8396f1df1e63d8c8d606f6d560131e5d0e5954ac
amukhin <bbyxW4vL8qNhqwkh>
2021-10-31 12:35:14 +0300

    add: csv to mongo script

---
+++ csv_to_mongo.py
@@ -0,0 +1,44 @@
+from csv import DictReader
+from datetime import datetime
+from os import listdir
+
+from pymongo import MongoClient
+
+from backend.helper import get_mongo_client
+
+
+def to_mongo(client: MongoClient, db_name: str, collection_name: str, notes: list[dict]):
+    collection_name = collection_name.replace('.csv', '')
+    db = client[db_name][collection_name]
+    db.insert_many(notes)
+
+
+def get_notes(reader: DictReader):
+    result = []
+    for line in reader:
+        for key, value in line.items():
+            try:
+                line[key] = int(value)
+            except ValueError:
+                pass
+
+        result.append(line)
+        if len(result) % 7000 == 0:
+            yield result
+            result.clear()
+    yield result
+
+
+if __name__ == '__main__':
+    client = get_mongo_client()
+    db_name = 'nosql'
+    source_dir_path = './source'
+
+    for file in listdir(source_dir_path):
+        if not file.endswith('.csv'):
+            continue
+        with open(f'{source_dir_path}/{file}', newline='', encoding='utf-8-sig') as csvfile:
+            reader = DictReader(csvfile)
+            for batch in get_notes(reader):
+                to_mongo(client, db_name, file, batch)
+                print(f'{datetime.utcnow()}: insert {len(batch)} notes of {file} collection')
