facfd3736a9b39f8645ad01173be91551198cf43
amukhin <bbyxW4vL8qNhqwkh>
2021-11-01 14:44:20 +0300

    update: helper file

--- backend/helper.py
+++ backend/helper.py
@@ -1,19 +1,11 @@
-import os
-from pymongo import MongoClient
-from dotenv import load_dotenv
+from pymongo import MongoClient, ASCENDING, DESCENDING
+from backend.settings import MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT, DB_NAME
 
 
-load_dotenv()
-
-
-def get_mongo_client():
-    mongo_user = os.environ.get('MONGO_INITDB_ROOT_USERNAME')
-    mongo_password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
-
-    assert mongo_user is not None
-    assert mongo_password is not None
-
-    mongo_host = os.environ.get('MONGO_HOST', '5.23.49.205')
-    mongo_port = os.environ.get('MONGO_PORT', 27017)
-    mongo_url = f'mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}'
+def get_mongo_client() -> MongoClient:
+    mongo_url = f'mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}'
     return MongoClient(mongo_url)
+
+
+async def get_direction(ascending: bool) -> int:
+    return ASCENDING if ascending else DESCENDING
