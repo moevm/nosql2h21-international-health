c8d540ae69ef078fe056c029379c52e53905c67d
amukhin <bbyxW4vL8qNhqwkh>
2021-11-19 02:54:11 +0300

    update: add method for import file

--- backend/helper.py
+++ backend/helper.py
@@ -1,5 +1,12 @@
-from pymongo import MongoClient, ASCENDING, DESCENDING
-from backend.settings import MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT, DB_NAME
+from csv import DictReader
+from typing import Union
+
+import pandas as pd
+from fastapi import HTTPException, status
+from pymongo import ASCENDING, DESCENDING, MongoClient
+
+from backend.settings import (DB_NAME, MONGODB_HOST, MONGODB_PASSWORD,
+                              MONGODB_PORT, MONGODB_USER)
 
 
 def get_mongo_client() -> MongoClient:
@@ -9,3 +16,43 @@ def get_mongo_client() -> MongoClient:
 
 async def get_direction(ascending: bool) -> int:
     return ASCENDING if ascending else DESCENDING
+
+
+def to_mongo(client: MongoClient, collection_name: str, notes: list[dict]):
+    db = client[DB_NAME][collection_name]
+    db.insert_many(notes)
+
+
+def get_batch(reader: Union[DictReader, list[dict]], n: int = 40000):
+    batch = []
+    for line in reader:
+        for key, value in line.items():
+            try:
+                line[key] = int(value)
+            except ValueError:
+                try:
+                    line[key] = float(value)
+                except ValueError:
+                    pass
+
+        batch.append(line)
+        if len(batch) % n == 0:
+            yield batch
+            batch.clear()
+    yield batch
+
+
+async def import_file(file, mongo_client: MongoClient, collection: str):
+    if mongo_client[DB_NAME][collection].count() != 0:
+        raise HTTPException(status_code=status.HTTP_208_ALREADY_REPORTED, detail='Collection already exists')
+    content = await file.read()
+    table_view = content.decode('utf-8-sig').split('\r\n')
+    if len(table_view) < 2:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Empty csv file')
+    data = [i.split(',') for i in table_view[1:]]
+    columns = table_view[0].split(',')
+    df = pd.DataFrame(data, columns=columns)
+    to_mongo_data = [vls for _, vls in df.iloc[:-1].to_dict('index').items()]
+    for batch in get_batch(to_mongo_data, 30000):
+        to_mongo(mongo_client, collection, batch)
+    return {'success': True}
